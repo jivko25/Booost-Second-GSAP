2b2e4c16d97bb9d2c35ee87219fc327d46e228b1
jivko25 <jivkou@yahoo.com>
2021-02-09 10:20:17 +0200

    fix

--- src/app/custom/Animation.js
+++ src/app/custom/Animation.js
@@ -1,17 +1,18 @@
 import Saucer from './Saucer';
 import Cow from './Cow';
+import EventEmitter from 'eventemitter3';
 
 export default class Animation{
     constructor(){
         this.saucer = new Saucer();
         this.cow = new Cow();
     }
-    start(){
-        this.saucer.moveTo();
-        this.saucer.BeamShow();
-        this.cow.moveTo();
-        this.cow.hide();
-        this.saucer.BeamHide();
-        this.saucer.moveAway();
+    async start(){
+        await this.saucer.moveTo();
+        await this.saucer.BeamShow();
+        await this.cow.moveTo();
+        await this.cow.hide();
+        await this.saucer.BeamHide();
+        await this.saucer.moveAway();
     }
 }
